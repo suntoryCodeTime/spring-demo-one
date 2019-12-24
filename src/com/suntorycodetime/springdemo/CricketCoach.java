1f7596e3a8c597b817f0e5d77ddabd675da00f6c
suntoryCodeTime <dldavidley@gmail.com>
2019-12-24 13:13:33 -0700

    Added Setter Dependency Injection

---
+++ src/com/suntorycodetime/springdemo/CricketCoach.java
@@ -0,0 +1,27 @@
+package com.suntorycodetime.springdemo;
+
+public class CricketCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	public CricketCoach() {
+		super();
+		System.out.println("CricketCoach: inside no-arg constructor");
+	}
+
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("CricketCoach: inside setter method");
+		this.fortuneService = fortuneService;
+	}
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Practice fast bowling for 15 minutes";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+}
--- src/com/suntorycodetime/springdemo/SetterDemoApp.java
+++ src/com/suntorycodetime/springdemo/SetterDemoApp.java
@@ -1,26 +1,22 @@
 package com.suntorycodetime.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /*
- * Utilizes Constructor Dependency Injection
+ * Utilizes Setter Dependency Injection
  * */
-public class HelloSpringApp {
+public class SetterDemoApp {
 
 	public static void main(String[] args) {
-		/* Load the spring config file. */
+		
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
-		/* Retrieve bean from spring container. */
-		Coach theCoach = context.getBean("myCoach", Coach.class);
+		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 		
-		/* Call methods on the bean. */
 		System.out.println(theCoach.getDailyWorkout());
-		
 		System.out.println(theCoach.getDailyFortune());
 		
-		/* CLose the context. */
 		context.close();
-		
 	}
 
 }
