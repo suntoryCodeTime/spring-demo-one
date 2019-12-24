8d76de887045a93841630543dacd48b32c0e9150
suntoryCodeTime <dldavidley@gmail.com>
2019-12-24 12:03:38 -0700

    Added applicationContext.xml to utilize spring beans

---
+++ src/com/suntorycodetime/springdemo/HelloSpringApp.java
@@ -0,0 +1,22 @@
+package com.suntorycodetime.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+		/* Load the spring config file. */
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		/* Retrieve bean from spring container. */
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		/* Call methods on the bean. */
+		System.out.println(theCoach.getDailyWorkout());
+		
+		/* CLose the context. */
+		context.close();
+		
+	}
+
+}
--- src/com/suntorycodetime/springdemo/MyApp.java
+++ src/com/suntorycodetime/springdemo/MyApp.java
@@ -3,6 +3,7 @@ package com.suntorycodetime.springdemo;
 public class MyApp {
 
 	public static void main(String[] args) {
+		
 		// Create the Object
 		Coach theCoach = new TrackCoach();
 		
